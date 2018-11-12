f103dfa8c8dc507feb2e7bbb6ac6ed03d327c229
John Lee <drumwolf@Johns-MacBook-Pro.local>
2018-11-11 20:59:13 -0800

    refactored repo into express js web app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const url = require('url');
+const app = express();
+const port = 3000;
+
+// USE HANDLEBARS FOR VIEWS
+// Register Handlebars view engine, set layout to 'layouts/main.hbs'
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', 'hbs');	// Use Handlebars view engine
+
+// ROUTES
+app.get('/', (req,res) => res.render('index') );
+
+app.listen(port, () => console.log(`Example app listening on port ${port}...`) );
